baae265ec0b3b0bc01612910d1357b2ed25a84cb
Jeremiah Strong <jeremiahstrong@eislett.com>
2025-05-31 02:27:35 -0400

    feat: added api get request route for questions

---
+++ src/pages/api/questions.ts
@@ -0,0 +1,65 @@
+import { HTTP_REQUEST_INTERVAL, HTTP_REQUEST_LIMIT } from "@/constants/rateLimitParams";
+import rateLimit from "@/services/rateLimit";
+import { formatGetReqJson } from "@/services/utils";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const isRateLimit = rateLimit(HTTP_REQUEST_LIMIT, HTTP_REQUEST_INTERVAL)
+  if( !(await isRateLimit(req , res) )){
+    return res.status(429).json({ error: 'Too many requests, please try again later.' } );
+  }
+  
+  console.log('GET /api/questions (App Router)');
+
+
+  const query = req.query ; // { page_number: '1', page_size: '5' }
+
+  console.log('query', req.query)
+
+  if (!query?.page_number) {
+    return res.status(400).json({ error: 'Page Number is required' });
+  }
+  else if (!query.page_size) {
+    return res.status(400).json({ error: 'Page Size is required' });
+  }
+
+
+  try {
+    const route = 'questions';
+    const token = req.headers.authorization;
+    const apiKey = process.env.API_KEY;
+    const nodeServer = process.env.SERVER_BASE_URL;
+
+    const rawResponse = await fetch(
+      `${nodeServer}${route}?${formatGetReqJson(query)}`,
+      {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          "Authorization" : token
+        },
+      }
+    );
+
+    if (!rawResponse.ok) {
+      throw new Error('Failed to fetch questions');
+    }
+
+    const response = await rawResponse.json()
+
+    // console.log('GET /api/questions (Response):', response);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({ error: 'Questions fetch: ' + error.message });
+  }
+}
+
+export const config = {
+  api: {
+    responseLimit: false,
+    bodyParser: true, // to parse data
+  },
+}
+
+export default GET;
